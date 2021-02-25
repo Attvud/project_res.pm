409ed5c9674288fd03c51c9f620f2c1393f3994f
IIIRes <bhjSEnkjd3@res.pm>
2021-02-25 11:47:03 +0200

    +

--- sw.js
+++ sw.js
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "aceecd74-b95e382f6d3583550ff4.js"
   },
   {
-    "url": "app-740555d39c32313b1f27.js"
+    "url": "app-a1076770bf700d0ffee1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4a4bc05a4d1c5b591d0f010b74389e60"
+    "revision": "19f60523bb197f23398a3f93349c826c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "91e72070db08424cb7865c12f9009b26"
+    "revision": "1cea50e284d332ea46cf539d4061c32e"
   },
   {
     "url": "polyfill-6c74939eb57bbd57b6a0.js"
@@ -66,57 +66,14 @@ self.__precacheManifest = [
     "url": "d99a64e8ab5703df30f0e0f475283d4270bcd548-29d022cd01e36d275cb1.js"
   },
   {
-    "url": "8f06df63790ccae6715eac2fe158efb9ccdbe7dd-d25e7c08aa5af20512a0.js"
-  },
-  {
-    "url": "component---src-projects-res-pm-components-pages-404-js-0ac72a19436b159ffff1.js"
-  },
-  {
-    "url": "page-data/404/page-data.json",
-    "revision": "3f9abae71810781a5f7333574119a529"
-  },
-  {
-    "url": "page-data/404.html/page-data.json",
-    "revision": "042780b256a947c37dfdf5cd33f562d5"
-  },
-  {
-    "url": "component---src-projects-res-pm-components-pages-contacts-js-af290dcf7555d44df865.js"
-  },
-  {
-    "url": "page-data/contacts/page-data.json",
-    "revision": "4fed99336d688dedbc03b5b1031e297e"
-  },
-  {
-    "url": "component---src-projects-res-pm-components-pages-index-js-c6d28049f9569242c3d7.js"
-  },
-  {
-    "url": "page-data/index/page-data.json",
-    "revision": "57f02aac4c387d09bfaa3be550e13ad5"
-  },
-  {
-    "url": "component---src-projects-res-pm-components-pages-portfolio-js-c49ca037b8d4cf728241.js"
-  },
-  {
-    "url": "page-data/portfolio/page-data.json",
-    "revision": "c8f840bbd2a5dfdc74460b4a6ad44f26"
-  },
-  {
-    "url": "page-data/sq/d/1419746806.json",
-    "revision": "102edc593d4b8146e3e2f80b119b1182"
-  },
-  {
-    "url": "component---src-projects-res-pm-components-pages-resume-js-34e7747a4820321f4b48.js"
-  },
-  {
-    "url": "page-data/resume/page-data.json",
-    "revision": "c82077d9d804cb0a11cbba9bfd5d6f3a"
-  },
-  {
     "url": "component---src-projects-res-pm-components-templates-shop-product-js-57ffaeafc57e25784780.js"
   },
   {
     "url": "page-data/shop/test/page-data.json",
     "revision": "0838d403426e0e4b7b72c6cc739767d0"
+  },
+  {
+    "url": "8f06df63790ccae6715eac2fe158efb9ccdbe7dd-d25e7c08aa5af20512a0.js"
   },
   {
     "url": "component---src-projects-res-pm-components-templates-portfolio-single-js-4a204fce5e3506dd6980.js"
@@ -160,6 +117,17 @@ self.__precacheManifest = [
   {
     "url": "page-data/portfolio/1614111961930/page-data.json",
     "revision": "ac8cea9dde0cf6936215eec9a2433325"
+  },
+  {
+    "url": "component---src-projects-res-pm-components-pages-portfolio-js-c49ca037b8d4cf728241.js"
+  },
+  {
+    "url": "page-data/portfolio/page-data.json",
+    "revision": "c8f840bbd2a5dfdc74460b4a6ad44f26"
+  },
+  {
+    "url": "page-data/sq/d/1419746806.json",
+    "revision": "102edc593d4b8146e3e2f80b119b1182"
   },
   {
     "url": "page-data/portfolio/test/page-data.json",
@@ -266,7 +234,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-740555d39c32313b1f27.js`))) {
+  if (!resources || !(await caches.match(`/app-a1076770bf700d0ffee1.js`))) {
     return await fetch(event.request)
   }
 
