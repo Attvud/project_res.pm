05491226d71456f3d0f875adefd616de0cd35ac4
IIIRes <bhjSEnkjd3@res.pm>
2021-03-06 07:29:03 +0200

    +

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8b3eeedc8bbfb9fdb174.js"
+    "url": "webpack-runtime-938bed8732524f4d0703.js"
   },
   {
     "url": "framework-b17a64ad725e0b3c1338.js"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "aceecd74-b95e382f6d3583550ff4.js"
   },
   {
-    "url": "app-f462cab247795515bf0a.js"
+    "url": "app-1f6ee0938ff060fd3e63.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "14b48ad10faf3542e0037ac178092584"
+    "revision": "9285b329b5ae3e5aa0ec3c701f6b583b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
@@ -57,26 +57,26 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "f217fbd9d1a16d346c364b0229d188f3"
+    "revision": "5348925ce9286b6411114da6b2dd0823"
   },
   {
     "url": "polyfill-6c74939eb57bbd57b6a0.js"
   },
   {
-    "url": "d99a64e8ab5703df30f0e0f475283d4270bcd548-54d8798b80962a3d2760.js"
+    "url": "d99a64e8ab5703df30f0e0f475283d4270bcd548-7dbf83540a6f274b1f63.js"
   },
   {
-    "url": "9b0684e80267a3c720b29fd2cecf031acdb1a309-247174f7b7c2db03f013.js"
+    "url": "9b0684e80267a3c720b29fd2cecf031acdb1a309-b3dc2284e379d290b23a.js"
   },
   {
-    "url": "component---src-projects-res-pm-components-pages-resume-js-1d64062102ae750c9419.js"
+    "url": "component---src-projects-res-pm-components-pages-resume-js-b0a8f295ccde00369139.js"
   },
   {
     "url": "page-data/resume/page-data.json",
     "revision": "c82077d9d804cb0a11cbba9bfd5d6f3a"
   },
   {
-    "url": "component---src-projects-res-pm-components-pages-portfolio-js-e753146981375d58f4d7.js"
+    "url": "component---src-projects-res-pm-components-pages-portfolio-js-6dee1d59c82c3f2d25ea.js"
   },
   {
     "url": "page-data/portfolio/page-data.json",
@@ -87,7 +87,7 @@ self.__precacheManifest = [
     "revision": "aa608b1ce47660fd5daf3abe455d4b56"
   },
   {
-    "url": "component---src-projects-res-pm-components-pages-contacts-js-5ef487524151d80bc3b8.js"
+    "url": "component---src-projects-res-pm-components-pages-contacts-js-01f5163dd84304835510.js"
   },
   {
     "url": "page-data/contacts/page-data.json",
@@ -187,7 +187,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-f462cab247795515bf0a.js`))) {
+  if (!resources || !(await caches.match(`/app-1f6ee0938ff060fd3e63.js`))) {
     return await fetch(event.request)
   }
 
