f2dc6d6f435244f7b6417b254ed58a4c2bf0752f
IIIRes <bhjSEnkjd3@res.pm>
2021-05-31 14:47:22 +0300

    +

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-45b44eb54a657a54a3d6.js"
+    "url": "webpack-runtime-677d0faee02a7c8a1d73.js"
   },
   {
     "url": "framework-151d16a262f39a1b5ad0.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1611eedbfab8ea19d51751da02db5d84"
+    "revision": "ebb2307e10d343d0821aa916ebf7aea0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-a5364d144488098a841a.js"
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "polyfill-d11fce74a104841521bf.js"
   },
   {
-    "url": "b84ed7e41dc01b88e6459bb98ae462c5df355fc6-e6f115a9bf1e90f084db.js"
+    "url": "b84ed7e41dc01b88e6459bb98ae462c5df355fc6-b0edb67a629e907b9b50.js"
   },
   {
     "url": "39f65ee59cfde5b1253e04e6aae3dbad88f56144-853e9dc9579578e68ee3.js"
@@ -66,7 +66,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "2c0f8e99afd83403434ccc275198a583"
+    "revision": "bb2629bd881d9019c621edcd690d9957"
   },
   {
     "url": "component---src-components-project-res-pm-pages-portfolio-js-d7efaf325aeefcb1a10a.js"
